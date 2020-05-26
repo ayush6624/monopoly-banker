40a78a2c78f885b482f388b9d9c179be4cb2176c
ayush6624 <ayushg1214@gmail.com>
2020-05-27 01:30:13 +0530

    feat: add sockets backend

--- server/github.js
+++ server/github.js
@@ -2,7 +2,8 @@ Array.prototype.search = function (key, value) {
   for (let i = 0; i < this.length; i++) {
     if (this[i][key] == value) return i;
   }
-
+  // can refer as players[player].id.....
+  // To check False retuens use Number.isNumber(...)
   return false;
 };
 
@@ -28,60 +29,70 @@ server.listen(4000);
 app.set('view engine', 'jade');
 
 io.on('connection', (socket) => {
-  console.log(socket.id);
-  socket.on('getPlayers', () => {
+  socket.on('getPlayers', (username) => {
+    console.log('Firing -> ', username);
     console.log(players);
-    socket.emit('system', { players });
-  });
-  socket.on('register', (username) => {
-    const player = players.search('username', username);
-    if (Number.isInteger(player)) {
-      players[player].id = socket.id;
-      socket.emit('balance', { balance: players[player].balance });
-    } else {
-      players.push({ id: socket.id, username: username, balance: 1500 });
-      socket.emit('registration', 'yeah');
+    try {
+      players[players.search('username', username)].id = socket.id;
+    } catch {
+      console.log('no socket id');
     }
-    console.log(players);
-    console.log(timestamp() + chalk.magenta('CONNECTION: ') + chalk.gray(username + ' connected'));
-    io.emit('update', players);
+
+    socket.emit('update', players);
+  });
+
+  socket.on('register', (username) => {
+    let player = players.search('username', username);
+    if (Number.isInteger(player)) {
+      socket.emit('notification', { type: 'error', message: 'Username Already Taken' });
+    } else {
+      // username = username.charAt(0).toUpperCase() + username.slice(1);
+      players.push({ id: socket.id, username: username, balance: 1500 });
+      io.sockets.emit('notification', { type: 'info', message: username + ' has joined the game' });
+      socket.emit('update', players);
+      console.log(players);
+      console.log(timestamp() + chalk.magenta('CONNECTION: ') + chalk.gray(username + ' connected'));
+      io.emit('update', players);
+    }
+  });
+
+  socket.on('disconnect', () => {
+    console.log('disconenect');
+    const player = players.search('id', socket.id);
+    if (Number.isInteger(player)) {
+      // Prevent duplicate notifications where user might click exit and then also close browser
+      // io.sockets.emit('notification', { type: 'info', message: players[player].username + ' has left the room' });
+    }
+  });
+
+  socket.on('message', (m) => {
+    if (m === 'exit_notification') {
+      try {
+        io.sockets.emit('notification', { type: 'error', message: players[players.search('id', socket.id)].username + ' has left the game' });
+      } catch {
+        console.log('cant find user in db');
+      }
+    } else io.sockets.emit('notification', { type: 'error', message: m });
+    console.log('p2p notification ');
   });
 
   socket.on('pay', (data) => {
     if (Number.isInteger(players.search('id', socket.id))) {
       if (Number.isInteger(data.value) && data.value < 10000) {
         players[players.search('id', socket.id)].balance -= parseInt(data.value);
-
         if (data.player == 'fine') {
           parking += parseInt(data.value);
-
-          socket.emit('balance', {
-            balance: players[players.search('id', socket.id)].balance,
-            message: '<i class="fa fa-minus"></i>' + data.value + ' to Free Parking',
-          });
-
+          io.sockets.emit('update', players);
+          io.sockets.emit('notification', { type: 'info', message: 'Fine Deducted' });
           console.log(timestamp() + chalk.yellow('TRANSFER: ') + chalk.gray(players[players.search('id', socket.id)].username + ' paid a £' + data.value + ' fine'));
-        } else if (data.player == 'bank') {
-          socket.emit('balance', {
-            balance: players[players.search('id', socket.id)].balance,
-            message: '<i class="fa fa-minus"></i>' + data.value + ' to the bank',
-          });
-
+        } else if (data.player === 'bank') {
+          io.sockets.emit('update', players);
+          io.sockets.emit('notification', { type: 'info', message: 'Bank Transfer Successful' });
           console.log(timestamp() + chalk.yellow('TRANSFER: ') + chalk.gray(players[players.search('id', socket.id)].username + ' paid £' + data.value + ' to the bank'));
         } else if (Number.isInteger(players.search('id', data.player))) {
           players[players.search('id', data.player)].balance += parseInt(data.value);
-
-          if (data.player in io.sockets.connected)
-            io.sockets.connected[data.player].emit('balance', {
-              balance: players[players.search('id', data.player)].balance,
-              message: '<i class="fa fa-plus"></i>' + data.value + ' from ' + players[players.search('id', socket.id)].username,
-            });
-
-          socket.emit('balance', {
-            balance: players[players.search('id', socket.id)].balance,
-            message: '<i class="fa fa-minus"></i>' + data.value + ' to ' + players[players.search('id', data.player)].username,
-          });
-
+          io.sockets.emit('update', players);
+          io.sockets.emit('notification', { type: 'info', message: 'Money Transfer' });
           console.log(timestamp() + chalk.yellow('TRANSFER: ') + chalk.gray(players[players.search('id', socket.id)].username + ' paid £' + data.value + ' to ' + players[players.search('id', data.player)].username));
         }
       }
@@ -103,27 +114,29 @@ io.on('connection', (socket) => {
     }
   });
 
-  socket.on('message', (command) => {
-    if (Number.isInteger(players.search('id', socket.id))) {
-      if (command == 'go') {
-        players[players.search('id', socket.id)].balance += 200;
+  // socket.on('message', (command) => {
+  //   if (Number.isInteger(players.search('id', socket.id))) {
+  //     if (command == 'go') {
+  //       players[players.search('id', socket.id)].balance += 200;
 
-        socket.emit('balance', {
-          balance: players[players.search('id', socket.id)].balance,
-          message: '<i class="fa fa-plus"></i>200 for passing Go',
-        });
+  //       socket.emit('balance', {
+  //         balance: players[players.search('id', socket.id)].balance,
+  //         message: '<i class="fa fa-plus"></i>200 for passing Go',
+  //       });
 
-        console.log(timestamp() + chalk.green('GO: ') + chalk.gray(players[players.search('id', socket.id)].username + ' passed Go'));
-      } else if (command == 'parking') {
-        players[players.search('id', socket.id)].balance += parking;
+  //       console.log(timestamp() + chalk.green('GO: ') + chalk.gray(players[players.search('id', socket.id)].username + ' passed Go'));
+  //     } else if (command == 'parking') {
+  //       players[players.search('id', socket.id)].balance += parking;
 
-        socket.emit('balance', {
-          balance: players[players.search('id', socket.id)].balance,
-          message: '<i class="fa fa-plus"></i>' + parking + ' from Free Parking',
-        });
+  //       socket.emit('balance', {
+  //         balance: players[players.search('id', socket.id)].balance,
+  //         message: '<i class="fa fa-plus"></i>' + parking + ' from Free Parking',
+  //       });
 
-        console.log(timestamp() + chalk.blue('FREE PARKING: ') + chalk.gray(players[players.search('id', socket.id)].username + ' collected Free Parking'));
-      }
-    }
-  });
+  //       console.log(timestamp() + chalk.blue('FREE PARKING: ') + chalk.gray(players[players.search('id', socket.id)].username + ' collected Free Parking'));
+  //     } else if (command === 'exit_notification') {
+  //       io.sockets.emit('notification', { type: 'error', message: players[players.search('id', socket.id)].username + ' has left the game' });
+  //     }
+  //   }
+  // });
 });
